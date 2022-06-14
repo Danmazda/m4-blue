918c5291c13564636f775e54828dde2adc80afb0
Danilo Procópio <85803705+Danmazda@users.noreply.github.com>
2022-06-14 21:07:32 +0000

    primeiro exercício

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,27 @@
-const nome: "Danilo" = "Danilo";
-console.log(nome);
+export type Profession = 'Backend developer' | 'Product Manager';
+
+export type User = {
+    name: string,
+    age: number,
+    occupation: Profession,
+};
+
+export const users: User[] = [
+    {
+        name: 'Wilker',
+        age: 25,
+        occupation: 'Backend developer'
+    },
+    {
+        name: 'Bob',
+        age: 23,
+        occupation: 'Product Manager'
+    }
+];
+
+export function logPerson(user: User) {
+    console.log(` - ${user.name}, ${user.age}`);
+}
+
+console.log('Users:');
+users.forEach(logPerson);
